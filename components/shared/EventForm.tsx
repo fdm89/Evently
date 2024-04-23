ce30b7d9b2b95442174a1d6923c846ec891d87ea
Fulvio Di Maio <fulvio.dimaio1@gmail.com>
2024-04-23 18:17:00 +0200

    berlin event

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -27,7 +27,6 @@ import { Checkbox } from "../ui/checkbox";
 import { useRouter } from "next/navigation";
 import { createEvent, updateEvent } from "@/lib/actions/event.actions";
 import { IEvent } from "@/lib/database/models/event.model";
-import { type } from "os";
 
 type EventFormProps = {
   userId: string;
